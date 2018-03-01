cfc7b0945c86c4144b0d5be436fe4862324db8a1
web2\Анатолий <CRARGER@yandex.ru>
2018-03-01 14:23:27 +0300

    залил новую франшизу для теста

--- app/js/bundle.js
+++
@@ -1,9 +0,0 @@
-//= ../libs/jquery/dist/jquery.min.js
-
-//= ../libs/bootstrap/dist/js/bootstrap.min.js
-
-//= ../libs/owl.carousel/dist/owl.carousel.min.js
-
-//= ../libs/jquery-countTo/jquery.countTo.js
-
-//= script.js
